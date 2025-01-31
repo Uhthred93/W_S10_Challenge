e592d4dc795f13d099e49f63bdbb26dfda2c4013
Michael <mmontalvo93@outlook.com>
2025-01-30 23:54:03 -0500

    Completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PizzaForm from './PizzaForm'
 import OrderList from './OrderList'
+import axios from 'axios'
 
 export default function App() {
+
+  const [ orders , setOrders ] = useState([])
+
+  const load = async () => {
+    const response = await axios.get( "http://localhost:9009/api/pizza/history")
+    console.log( response )
+    setOrders( response.data )
+  }
+
+  useEffect( () => {
+    load();
+  },[])
+
   return (
     <div id="app">
-      <PizzaForm />
-      <OrderList />
+      <PizzaForm onOrderPost={load}/>
+      <OrderList orders={orders}/>
     </div>
   )
 }
--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-export default function OrderList() {
-  const orders = []
+export default function OrderList({ orders }) {
+
+  
+  const [filter, setFilter] = useState("All")
+
+  const handleFilter = (size) => {
+    setFilter( size )
+  }
+
   return (
     <div id="orderList">
       <h2>Pizza Orders</h2>
       <ol>
         {
-          orders.map(() => {
+          orders.map((order, index) => {
+
+            if ( order.size != filter && filter != 'All') return;
+
+            const message  = order.customer + " ordered a size " + order.size + " with " + ( order.toppings ? order.toppings.length : 'no') + " toppings"
             return (
-              <li key={1}>
+              <li key={index}>
                 <div>
-                  order details here
+                  {message}
                 </div>
               </li>
             )
@@ -24,6 +36,7 @@ export default function OrderList() {
           ['All', 'S', 'M', 'L'].map(size => {
             const className = `button-filter${size === 'All' ? ' active' : ''}`
             return <button
+              onClick={() => handleFilter(size)}
               data-testid={`filterBtn${size}`}
               className={className}
               key={size}>{size}</button>
@@ -33,3 +46,4 @@ export default function OrderList() {
     </div>
   )
 }
+
